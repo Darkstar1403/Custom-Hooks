066f81fe3c6c270b029f29112a342de710c7a7b7
branco <branco.musich4803@alumnos.udg.mx>
2021-08-12 23:25:15 -0500

    First commit

---
+++ helpers/procesoPesado.js
@@ -0,0 +1,6 @@
+export const procesoPesado = (iteraciones) =>{
+    for(let i=0;i<iteraciones; i++){
+        console.log('otra vuelta')
+    }
+    return `${iteraciones} iteraciones realizadas`
+}
